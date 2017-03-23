90d4d667222f1c8308b1f821bff05467d2d131be
System <none>
2017-03-23 22:41:12 +0000

    ⛵️👨‍❤️‍👨 Updated with Glitch

---
+++ public/helper.js
@@ -0,0 +1,14 @@
+	
+function resizeIFrameToFitContent( iFrame ) {
+    console.log('iFrame', iFrame.id, iFrame.height);
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+
+window.addEventListener('DOMContentReady', function(e) {
+    // or, to resize all iframes:
+    var iframes = document.querySelectorAll("iframe");
+    for( var i = 0; i < iframes.length; i++) {
+        resizeIFrameToFitContent( iframes[i] );
+    }
+} );
